e7a1e365cac98130a2166b4f325c893d30db13ae
mterranova-pp <mterranova@hbu.co>
2023-04-08 11:31:10 -0400

    added sample K8s deployment with single node/nginx

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/michael/Downloads/mt-project-2022-6f736ed2546e.json")
+
+  project = "mt-project-2022"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
