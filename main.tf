81a9bbeb3eb341e13bc71b82d1503f561dffdfec
mterranova-pp <mterranova@hbu.co>
2023-04-08 19:09:56 -0400

    update provider

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/Users/michael/Downloads/mt-project-2022-6f736ed2546e.json")
 
   project = "mt-project-2022"
   region  = "us-central1"
