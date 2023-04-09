fa6f349f4675a05057c06c2273ded278f58056ad
michael2893 <terranova.7@osu.edu>
2023-04-09 11:13:49 -0400

    Test new fw rules with GKE (#12)
    * add GKE, FW rules, VPC 
    Co-authored-by: michael2893

--- gke.tf
+++ gke.tf
@@ -1,43 +1,43 @@
-# # GKE cluster
-# resource "google_container_cluster" "primary" {
-#   name     = "${var.project_id}-gke"
-#   location = "us-east4-c"
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = "us-east4-c"
 
-#   # We can't create a cluster with no node pool defined, but we want to only use
-#   # separately managed node pools. So we create the smallest possible default
-#   # node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 2
 
-#   network    = google_compute_network.vpc.name
-#   subnetwork = google_compute_subnetwork.subnet.name
-# }
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
 
-# # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = google_container_cluster.primary.name
-#   location   = "us-east4-c"
-#   cluster    = google_container_cluster.primary.name
-#   node_count = var.gke_num_nodes
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = "us-east4-c"
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
 
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-#     disk_size_gb = 10
-#     labels = {
-#       env = var.project_id
-#     }
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+    disk_size_gb = 10
+    labels = {
+      env = var.project_id
+    }
 
-#     # preemptible  = true
-#     machine_type = "e2-small"
-#     tags         = ["gke-node", "${var.project_id}-gke"]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
+    # preemptible  = true
+    machine_type = "e2-small"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
 
-#   }
-# }
+  }
+}
 
 
--- var.tf
+++ var.tf
@@ -1,25 +1,25 @@
-# variable "project_id" {
-#   default = "mt-project-2022"
-#   description = "project"
-# }
+variable "project_id" {
+  default     = "mt-project-2022"
+  description = "project"
+}
 
-# variable "region" {
-#   default = "us-east4"
-#   description = "region"
-# }
+variable "region" {
+  default     = "us-east4"
+  description = "region"
+}
 
 
-# variable "gke_username" {
-#   default     = ""
-#   description = "gke username"
-# }
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
 
-# variable "gke_password" {
-#   default     = ""
-#   description = "gke password"
-# }
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
 
-# variable "gke_num_nodes" {
-#   default     = 1
-#   description = "number of gke nodes"
-# }
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
